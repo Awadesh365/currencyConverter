7e1ed5620055f9672d052e5d5b62abeaed7da957
Awadesh <36awadesh@gmail.com>
2024-06-20 15:51:34 +0530

    Design the custom hook

---
+++ src/hooks/useCurrencyInfo.js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+export default function useCurrencyInfo(currency) {
+
+    const [data, setData] = useState({});
+
+    useEffect(() => {
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024.6.11/v1/currencies/${currency}.json`)
+            .then((res) => res.json())
+            .then((res) => setData(res[currency]))
+        console.table(data);
+    }, [currency])
+
+    console.log(data);
+    return data;
+}
