0d7862ee6c26d0a329787f5eba347bf87144901a
Awadesh <36awadesh@gmail.com>
2024-06-20 16:34:25 +0530

    usage of useId hook for optimization and addition of singular index.js for all export components

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useId } from "react";
 
 function InputBox({
   label,
@@ -12,11 +12,19 @@ function InputBox({
 
   className = "",
 }) {
+  const amountInputId = useId();
+
   return (
     <div className={`bg-white p-3 rounded-lg text-sm flex ${className} `}>
       <div className="w-1/2">
-        <label className="text-black/40 mb-2 inline-block">{label}</label>
+        <label
+          htmlFor={amountInputId}
+          className="text-black/40 mb-2 inline-block"
+        >
+          {label}
+        </label>
         <input
+          id={amountInputId}
           className="outline-none w-full bg-transparent py-1.5"
           type="number"
           placeholder="Amount"
